d9aec28b5a4af6dc3b3211326a89952f592c36d9
Jake <JakeShoffner27@gmail.com>
2022-07-04 17:51:45 -0400

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -61,7 +61,7 @@ const Projects = () => {
               <img src={Superball} alt='superball project'/>
               <div className='flex flex-row justify-around'>
                 <h1 onClick={() => setAnInfo(!anomieInfo)} className="hidden md:flex md:my-5 hover:text-[#00C896] cursor-pointer transition ease-in-out duration-250">Info</h1>
-                <a href="https://jxk0be.me/" target="_blank" rel="noreferrer" className="md:my-5 my-1 hover:text-[#00C896] cursor-pointer transition ease-in-out duration-250">Github</a>
+                <a href="https://github.com/Jxk0be/SB-High-Score-Challenge" target="_blank" rel="noreferrer" className="md:my-5 my-1 hover:text-[#00C896] cursor-pointer transition ease-in-out duration-250">Github</a>
               </div>
             </div>
 
