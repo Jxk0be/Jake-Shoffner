8d681bd33176e08e1b45bf9bb30006ba254ad2de
Jake <JakeShoffner27@gmail.com>
2022-08-09 23:53:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-
+/* 
+  RUN: 'npm run deploy' 
+  every time you want to update the publication of the website
+*/
 function App() {
   return (
     <div>
