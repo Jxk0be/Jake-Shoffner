fd15f5798327511f6836647c8ce238daa7bfb6e9
Jake <JakeShoffner27@gmail.com>
2022-08-10 00:32:56 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     <div>
       <div id="about" className="w-full h-screen text-white text-2xl flex flex-col justify-center items-center">
       <div className='w-full h-screen flex text-[#dad8d8] flex-col text-xl justify-center items-center z-1'>
-        <div className='md:text-4xl uppercase font-semibold mb-6 md:mb-12 text-3xl'><h1><span className='text-[#00C896]'>0001:</span> About</h1></div>
+        <div className='md:text-4xl uppercase font-semibold mb-4 md:mb-12 text-3xl'><h1><span className='text-[#00C896]'>0001:</span> About</h1></div>
         <div className='grid grid-cols-1 md:grid-cols-2 md:gap-x-20 md:gap-y-20 md:w-4/5 w-4/5 gap-y-5 lg:w-3/5 md:text-2xl'>
           <div>
             I'm Jake. I'm a senior in computer science and statistics at the
